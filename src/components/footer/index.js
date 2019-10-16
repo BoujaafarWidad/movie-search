4a1529d3564e7fdbfcfcf4faea2f41074403bac8
widad <boujaafar58@gmail.com>
2019-10-16 15:10:28 +0100

    add footer

---
+++ src/components/footer/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./css/index.css";
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <p className="copyright">© GROUP TITLE 2019</p>
+      </footer>
+    );
+  }
+}
+export default Footer;
